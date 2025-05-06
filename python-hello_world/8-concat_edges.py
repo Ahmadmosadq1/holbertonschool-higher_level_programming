2cf4b28ca245dbf17c0211bc946bbff922c8e34a
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-06 11:20:29 +0000

    Task#6: adding comments

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
+# indexing to concat words from the string seperated by spacese
 str = str[39:54] + " " + str[55:66] + " " + str[107:111] + " " + str[0:6]
 print(str)
