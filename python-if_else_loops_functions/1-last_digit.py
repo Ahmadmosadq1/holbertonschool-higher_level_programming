d16f3d73199250ff9fe8ce36784a215bf34c2fbb
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-07 10:24:10 +0000

    Task#1: last digit

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+# We need to first convert the number to a string
+# So it can be indexed. then we get the last element.
+# Then, we convert the last element to integer and compare.
+num_str = str(number)
+last_digit = int(num_str[-1])
+
+if last_digit > 5:
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+
+elif last_digit ==  0:
+    print(f"Last digit of {number} is {last_digit} and is 0")
+
+elif last_digit < 6:
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
