7feba37a16bf71aae060c2c0e1a96dad62bfac7e
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-07 23:17:29 +0000

    using abs funtion instead of indexing

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
-# We need to first convert the number to a string
-# So it can be indexed. then we get the last element.
-# Then, we convert the last element to integer and compare.
-num_str = str(number)
-last_digit = int(num_str[-1])
-
+last_digit = abs(number) % 10
 
 if last_digit < 6 and last_digit != 0:
-    print(f"Last digit of {number} is {- last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {-last_digit} and is less than 6 and not 0")
 
 elif last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
