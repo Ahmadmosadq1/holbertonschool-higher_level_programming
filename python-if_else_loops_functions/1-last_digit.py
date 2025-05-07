1175a2ee2bdebf001e52661603eb35b8e9b51859
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-07 23:44:56 +0000

    fixing pycode style

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ if number < 0:
     last_dig = -last_dig
 
 if last_dig < 6 and last_dig != 0:
-    print(f"Last digit of {number} is {last_dig} and is less than6 and not 0")
+    print(f"Last digit of {number} is {last_dig} and is less than 6 and not 0")
 
 elif last_dig > 5:
     print(f"Last digit of {number} is {last_dig} and is greater than 5")
