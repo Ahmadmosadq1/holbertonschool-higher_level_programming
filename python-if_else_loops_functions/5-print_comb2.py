e9339e849cb2155be46f53eb89b4383633ce1e11
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-08 21:26:43 +0000

    prtinting the numbers with 2 significant figures

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,8 +2,8 @@
 
 # Looping from 0 to 100 as 100 will not be included.
 # If count reached the last number (i.e 99), it prints newline.
-for count in range(0, 100):
+for count in range(00, 100):
     if count == 99:
-        print("{}".format(count), end="\n")
+        print("{:02d}".format(count), end="\n")
         break
-    print("{}, ".format(count), end="")
+    print("{:02d}, ".format(count), end="")
