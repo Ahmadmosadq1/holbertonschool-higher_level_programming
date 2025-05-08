31748c3cef93e44c1d9a89f7f41b88c3674db7a9
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-08 21:46:56 +0000

    using comprhension expression and one print funtion

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 # Looping from 0 to 100 as 100 will not be included.
-# If count reached the last number (i.e 99), it prints newline.
-for count in range(00, 100):
-    if count == 99:
-        print("{:02d}".format(count), end="\n")
-        break
-    print("{:02d}, ".format(count), end="")
+# Using Comprhension expression, we loop from 0 to 99
+
+numbers = [f"{count:02d}" for count in range(100)]
+print(", ".join(numbers), end="\n")
