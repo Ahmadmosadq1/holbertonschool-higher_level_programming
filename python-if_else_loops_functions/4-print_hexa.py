e5949c973f978315f93001e3507504d6924f6a04
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-08 20:57:13 +0000

    hexa fromat correction

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -2,5 +2,5 @@
 
 # Looping through the decimal and hexa number and arrange them in colums
 for number in range(0,99):
-    print("{number:d} = 0x{number:X}".format(number = number))
+    print("{number:d} = {number:#x}".format(number = number))
 
