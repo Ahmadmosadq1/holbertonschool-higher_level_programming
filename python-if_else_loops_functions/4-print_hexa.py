1211b344fe428aa127501d19d3871018a7a8ffeb
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-08 02:53:57 +0000

    Sloving Task#4: looping in decimal and hexa

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -2,5 +2,5 @@
 
 # Looping through the decimal and hexa number and arrange them in colums
 for number in range(0,99):
-    print("{number:3d} = {number:3#X}".format(number, number))
+    print("{number:d} = 0x{number:X}".format(number = number))
 
