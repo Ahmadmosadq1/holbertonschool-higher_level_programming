9b798967508adc8d5585c846d94029e4dab5e203
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-08 22:05:59 +0000

    using str.format() method

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@
 # Looping from 0 to 100 as 100 will not be included.
 
 for count in range(100):
-    print(f"{count:02d}", end=", " if count < 99 else "\n")
+    print("{:02d}".format(count), end=", " if count < 99 else "\n")
