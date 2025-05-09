f5750b917240d80e6e9718b067aff28dc35e14dc
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-09 09:32:32 +0000

    fixing pycodestyle

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    
+
     # number % 10 operation gets the last digit
-    last_digit = abs( number)  % 10
+    last_digit = abs(number) % 10
     print("{}".format(last_digit), end="")
     return last_digit
