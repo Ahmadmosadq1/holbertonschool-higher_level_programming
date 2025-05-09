485ca9e7eff7b9b31f7fc75615e88c5581374a14
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-09 00:20:51 +0000

    added a peace before newline

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for num1 in range(9):
     for num2 in range(num1 + 1, 10):
         if (num1 == 8 and num2 == 9):
-            print("{}{}\n".format(num1, num2))
+            print("{}{} \n".format(num1, num2))
             break
         print("{}{}".format(num1, num2), end=", ")
