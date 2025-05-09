d301997065d787f1f6c43713aa3409be4feeed59
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-09 09:09:51 +0000

    using one print, it only prints one '\n'

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,11 +3,9 @@
 # converting the string to uppercase
 def uppercase(str):
     result = ""
-    for index, char in enumerate(str):
+    for char in str:
         if 'a' <= char <= 'z':
             result += chr(ord(char) - 32)
         else:
             result += char
-    print("{}".format(result), end="")
-    if index == len(str) - 1:
-        print("\n")
+    print("{}".format(result))
