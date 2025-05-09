f13d7c9d8efd9ea2cb8f075114dfb17f21e339e4
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-09 09:30:54 +0000

    adding print line

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,4 +3,6 @@
 def print_last_digit(number):
     
     # number % 10 operation gets the last digit
-    return abs( number)  % 10
+    last_digit = abs( number)  % 10
+    print("{}".format(last_digit), end="")
+    return last_digit
