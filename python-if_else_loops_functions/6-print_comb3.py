87dee14124766eea92f06dfd6e7658124e3adbfd
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-09 00:12:58 +0000

    Task#6: combination with no repetion in conting

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for num1 in range(9):
+    for num2 in range(num1, 10):
+        if (num1 == 8 and num2 == 9):
+            print("{}{}\n".format(num1, num2))
+            break
+        print("{}{}".format(num1, num2), end=", ")
