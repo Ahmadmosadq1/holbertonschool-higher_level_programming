bca3f7ed4d2a4ac0f80d7e5236d21e995d6cd78f
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-10 02:38:48 +0000

    using from instead of importing the whole ,odule

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 # since we will be using all the funtions, no need to use "from"
-import calculator_1
+from  calculator_1 import add, sub, mul, div
 
 # making a gaurd for the library
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{} + {} == {}".format(a, b, calculator_1.add(a, b)))
-    print("{} - {} == {}".format(a, b, calculator_1.sub(a, b)))
-    print("{} * {} == {}".format(a, b, calculator_1.mul(a, b)))
-    print("{} / {} == {}".format(a, b, calculator_1.div(a, b)))
+    print("{} + {} == {}".format(a, b, add(a, b)))
+    print("{} - {} == {}".format(a, b, sub(a, b)))
+    print("{} * {} == {}".format(a, b, mul(a, b)))
+    print("{} / {} == {}".format(a, b, div(a, b)))
