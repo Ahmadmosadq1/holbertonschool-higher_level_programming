f9d7fcb6bbe8445c8fb35643162da35ef0818eaa
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-10 09:10:53 +0000

    Task#4: spec and loader

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import importlib.util
+import sys
+
+
+def hidden_discovery():
+    # we first load the compiled module froma /tmp/ path
+    spec = importlib.util.spec_from_file_location("4_hidden", "hidden_4.pyc")
+    hidden = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(hidden)
+
+    # Extracting all the names defined in that bytecode file
+    names = dir(hidden)
+    # we now filter out any string starts with '__'
+    # if it was found, the if conditon will be True and with not, it skips
+    # if it was false, with 'not', it will be True and print them
+    filter_out = [n for n in names if not n.startswith("__")]
+    # sorting the names:
+    filter_out.sort()
+    # Now, we print each name in a line
+    for line in filter_out:
+        print("{}".format(line))
+
+
+if __name__ == "__main__":
+    hidden_discovery()
