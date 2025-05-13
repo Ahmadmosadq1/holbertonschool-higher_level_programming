65b61a8898b1143c821263394ebfa91c84593030
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-13 18:36:51 +0000

    Task#7: adding tuple elemnts

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,10 +6,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     for idx in range(2):
         # for each elemnt, assigned it to its repective index
         # if it is out of range, assigned to 0
-        a = tuple_a[idx] if idx < len(tuple_a) else 0 
-        b = tuple_a[idx] if idx < len(tuple_b) else 0
+        a = tuple_a[idx] if idx < len(tuple_a) else 0
+        b = tuple_b[idx] if idx < len(tuple_b) else 0
         # concating two elemnts in a new_list
         my_list[idx] = a + b
     # convert the list to tuple
     return tuple(my_list)
-    
