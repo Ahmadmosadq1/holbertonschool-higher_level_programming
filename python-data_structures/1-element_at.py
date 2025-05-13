28318107cbf47482d82ad5e98296e0e5e0863956
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-13 15:25:05 +0000

    subtracting list lingeth by one when comparing to idx

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,8 @@
 def element_at(my_list, idx):
 
     # It is the safest way to check if idx out of range as a first condition
-    if idx > len(my_list):
+    # beacuse index starts @ 0, we have to subtract list length by -1
+    if idx > len(my_list) - 1:
         return None
         # you use "return"  only without None.
 
