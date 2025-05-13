4977ce37baa49122e21ee871b123184f890a6e31
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-13 18:59:48 +0000

    Task#8: nothing major

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,9 +2,11 @@
 def multiple_returns(sentence):
     # if thr sentence is not empty
     str_length = len(sentence)
+
     if str_length == 0:
         first_char = None
     else:
-        first = sentence[0]
+        first_char = sentence[0]
     # we tuple over a list since tuple accepts only  argument
     return (str_length, first_char)
+    
