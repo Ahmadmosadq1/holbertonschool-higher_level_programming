75593c3d3d55000007c9a81224a75d28af42b0dc
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-13 18:04:37 +0000

    adding comments and pycodstyle correction

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    # we iterate each list inside the matrix
     for row in matrix:
+        # Here, we seperate the elements using join to iterate each list
+        # At the end of each loop, print() will add new line
         print(" ".join("{:d}".format(num) for num in row))
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
