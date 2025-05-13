8269bacfcf4c01fc2d0503fc454a6636a126c682
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-13 16:12:13 +0000

    Task#3: reversing a list

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    # Creating an empty list
+    new_list = []
+    # lopping through my_list indexes
+    for idx in my_list:
+        # using insert method to insert each idx at 0 position
+        new_list.insert(0, idx)
+    print(new_list)
