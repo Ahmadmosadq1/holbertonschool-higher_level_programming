1d6949ab78450dd2dd80d6aeb94da5dbaf7075d4
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-13 16:15:16 +0000

    adding :d format

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -6,4 +6,4 @@ def print_reversed_list_integer(my_list=[]):
     for idx in my_list:
         # using insert method to insert each idx at 0 position
         new_list.insert(0, idx)
-    print("{}".format(new_list))
+    print("{:d}".format(new_list))
