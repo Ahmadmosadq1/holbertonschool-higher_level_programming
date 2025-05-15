b97a7b6e5e3fb983b8308dc31b6aad6f107e5965
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-15 07:43:00 +0000

    kepping matrix by making a copy of it

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            matrix[i][j] = matrix[i][j] ** 2
+    copy_matrix = [row[:] for row in matrix]
+    for i in range(len(copy_matrix)):
+        for j in range(len(copy_matrix[i])):
+            copy_matrix[i][j] = copy_matrix[i][j] ** 2
+    return copy_matrix
