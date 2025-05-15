490a9ed3ba90b6f6defc395f965ac5df53b97af9
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-15 07:48:55 +0000

    adding comments

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    # doing a copy so we dont change the original
+    # [:] means copying the whole matrix
     copy_matrix = [row[:] for row in matrix]
+    # i loops though the lists, j loops through the elemnts of each list
     for i in range(len(copy_matrix)):
         for j in range(len(copy_matrix[i])):
             copy_matrix[i][j] = copy_matrix[i][j] ** 2
