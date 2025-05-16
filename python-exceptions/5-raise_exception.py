5bc62fc790ed0197fc7fb9e53ca86fd754fd4293
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-16 09:09:38 +0000

    Task#5: raise error handling

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def raise_exception():
+    # The differance between rasie and except is that except stops program
+    # whereas the except, if it was invoked, it will stop the program if it
+    # was not the last instruction
+    # rasie will give the user a chance to know where excatly the error is
+    raise TypeError
