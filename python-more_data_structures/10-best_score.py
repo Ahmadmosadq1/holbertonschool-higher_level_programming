9cdf6598d99fd168cc46150c7822f4e80b08c855
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-16 00:17:02 +0000

    Task#10: fidning the bigest value in a dictionary

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    # If no score found, return None
+    if not a_dictionary:
+        return None
+    # we first assume any key and its value is the biggest.
+    # We need to first turn them into list so we can index them properly
+    keys = list(a_dictionary)
+    # Chossing the first value-key pair to be the biggiest
+    biggest_key = keys[0]
+    biggest_value = a_dictionary[biggest_key]
+
+    # Looping through the keys and values
+    for key, value in a_dictionary.items():
+        if value > biggest_value:
+            biggest_value = value
+            biggest_key = key
+    return value
