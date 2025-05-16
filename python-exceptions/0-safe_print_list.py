030cb895965aba7085db40f6111a7d69584d6f98
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-16 06:42:15 +0000

    Task#0: exceptions for IndexError

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+def safe_print_list(my_list=[], x=0):
+    # Intitial cpunt
+    count = 0
+    for element in range(x):
+        # We use the try statment incase the proccess is susseful
+        try:
+            print("{}".format(my_list[element]), end="")
+            count += 1
+            # The exception encounters the error for out of range index
+            # it will break out of the loop if error occured
+        except IndexError:
+            break
+        print()
+        return count
