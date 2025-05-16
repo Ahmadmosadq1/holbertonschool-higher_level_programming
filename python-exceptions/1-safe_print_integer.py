05bd3cd206a9ab46ff56d9afd1ce99b1df99a616
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-16 06:59:04 +0000

    1-safe_print_integer.py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    # I actually had to run the code without Try/except so i can find 
+    # I actually had to run the code without Try/except so i can find
     # the error when i put a string input
     # if the code seceesed, excute the "try" block, otherwise, excpet block
     try:
