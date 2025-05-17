226856c49042b3bac6f78352dbb335c16221dfdf
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-17 08:09:19 +0000

    putting the error condition blocks at the begining to catch the error

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -25,13 +25,6 @@ def matrix_divided(matrix, div):
         "ZeroDivisionError": If div = 0
 
     """
-    new_list = []
-    for i in range(len(matrix)):
-        temp_list = []
-        for j in range(len(matrix[i])):
-            result = round(matrix[i][j]/div, 2)
-            temp_list.append(result)
-        new_list.append(temp_list)
     if (
         not isinstance(matrix, list) or
         not all(isinstance(row, list) for row in matrix) or
@@ -39,7 +32,8 @@ def matrix_divided(matrix, div):
             isinstance(element, (int, float))
             for row in matrix for element in row)
        ):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        text: "matrix must be a matrix (list of lists) of integers/floats"
+        raise TypeError(test)
     row_list = len(matrix[0])
     if not all(len(row) == row_list for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
@@ -47,4 +41,13 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+
+    new_list = []
+    for i in range(len(matrix)):
+        temp_list = []
+        for j in range(len(matrix[i])):
+            result = round(matrix[i][j]/div, 2)
+            temp_list.append(result)
+        new_list.append(temp_list)
+
     return new_list
