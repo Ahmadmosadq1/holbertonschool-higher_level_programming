5e30f2f25f6508ffc67faedef4ad569f536b475d
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-17 05:07:30 +0000

    fixing pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,22 +3,24 @@
 This module does the addition operation of arguments a and b
 The funcion also converts a float to an integr
 """
+
+
 def add_integer(a, b=98):
     """ Addition function that takes 2 arguments and adds them.
 
     Args:
         a: First number (integer or float)
         b: Second number (integer or float)
-    
+
     Returns:
         int: the sum of the two arguments
 
     Raises:
         if a or b is not an integer or float
     """
-    if not isinstance(a, (int,float)): # checks the type
+    if not isinstance(a, (int, float)):  # checks the type
         raise TypeError("a must be an integer")
-    if not isinstance(b,(int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     try:
         return int(a) + int(b)
