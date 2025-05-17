a9d6a35835e3de852b2fe1bda7912f35a8cd323c
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-17 07:55:18 +0000

    Task#1: matrix divided by a number with the error eceptions

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+This module has a function that takes 2 args.
+one is the matrix and the other is the diivider (div)
+it divides the matrix by div
+taking into account all the exception errors like TypeErrors..etc
+"""
+
+
+def matrix_divided(matrix, div):
+    """ Divider function that takes 2 arguments matrix and div.
+
+        Args:
+        matrix: list of lists of numbers(integer or float)
+        div: The divider (non zero)
+
+    Returns:
+        list: tnew_list
+
+    Raises:
+        "TypeError": if matrix is not list of list
+        or rows are not the same length
+        or the numbers are not float or int
+
+        "ZeroDivisionError": If div = 0
+
+    """
+    new_list = []
+    for i in range(len(matrix)):
+        temp_list = []
+        for j in range(len(matrix[i])):
+            result = round(matrix[i][j]/div, 2)
+            temp_list.append(result)
+        new_list.append(temp_list)
+    if (
+        not isinstance(matrix, list) or
+        not all(isinstance(row, list) for row in matrix) or
+        not all(
+            isinstance(element, (int, float))
+            for row in matrix for element in row)
+       ):
+        raise("matrix must be a matrix (list of lists) of integers/floats")
+    row_list = len(matrix[0])
+    if not all(len(row) == row_list for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (float, int)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return new_list
