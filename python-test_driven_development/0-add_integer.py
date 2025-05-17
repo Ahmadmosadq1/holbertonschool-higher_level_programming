9cb763f4f92c33ad0ad167478875e69da121a13d
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-17 02:09:03 +0000

    Task#0: Addition function

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """ Addition function that takes 2 arguments and adds them.
+
+    Returns:
+        int: the sum of the two arguments
+
+    Raises:
+        if a or b is not an integer or float
+    """
+    if not isinstance(a, (int,float)): # checks the type
+        raise TypeError("a must be an integer")
+    if not isinstance(b,(int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
