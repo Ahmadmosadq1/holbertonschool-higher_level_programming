9d82f1978a2202ffcc70a13141eefca1dda0023d
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-18 08:28:49 +0000

    adding more testes

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,10 +1,7 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-
 class TestMaxInteger(unittest.TestCase):
-    """Unit tests for max_integer()."""
-
     def test_empty_list(self):
         self.assertIsNone(max_integer([]))
 
@@ -13,6 +10,18 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_multiple_elements(self):
         self.assertEqual(max_integer([1, 3, 2, 4]), 4)
+
+    def test_max_at_beginning(self):
+        self.assertEqual(max_integer([9, 1, 2, 3]), 9)
+
+    def test_max_in_middle(self):
+        self.assertEqual(max_integer([1, 9, 2, 3]), 9)
+
+    def test_one_negative_number(self):
+        self.assertEqual(max_integer([-1, 0, 1, 2]), 2)
+
+    def test_only_negative_numbers(self):
+        self.assertEqual(max_integer([-5, -2, -8, -3]), -2)
 
 if __name__ == "__main__":
     unittest.main()
