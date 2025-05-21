8e4e366ece5018f723828b222299672acc5f1360
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-21 21:56:37 +0000

    intendation issue

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,17 +2,17 @@
 """Creating a class that defines a square"""
 
 
-class square:
+class Square:
     """This class has one private attribute which is size
-    
+
     Attributes:
         size (int): Represents the size of the square.
     """
     def __init__(self, size):
         """
-        This is an intance initiolizer 
-        
-        
+        This is an intance initiolizer
+
+
         Arguement:
         Size: the square size
     """
