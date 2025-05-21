1e8a3e040fe6cf932c1ec85fc47f257d2a110151
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-21 21:48:35 +0000

    adding documentation for init funtion

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -9,4 +9,10 @@ class square:
         size (int): Represents the size of the square.
     """
     def __init__(self, size):
+        """
+        This is an intance initiolizer 
         self.__size = size
+        
+        Arguement:
+        Size: the square size
+    """
