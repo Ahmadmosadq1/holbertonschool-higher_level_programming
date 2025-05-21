179fbfe011940e1b3645ae636defde48b32e892e
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-21 16:21:32 +0000

    Task#0: Creating an empty class

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """An empty class
+     Note:
+        None.
+
+    Args:
+       No arguments
+
+    Attributes:
+        no attributes
+        """
+    pass
