788a19312e7909be1e7550ac4d2452560f63b38f
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-21 21:38:18 +0000

    Task#1: adding class private inheratance

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Creating a class that defines a square"""
+
+
+class square:
+    """This class has one private attribute which is size
+    
+    Attributes:
+        size (int): Represents the size of the square.
+    """
+    def __init__(self, size):
+        self.__size = size
