3ff90489e6a9e3b5cfea4bf78404e85cfc2ad191
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-23 08:09:03 +0000

    Missing _ for a private variable

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -65,4 +65,16 @@ class Rectangle:
         """ multipling width by # in a row and looping through all loops
         in an (N)height
         """
-        return "\n".join("#" * self._width for _ in range(self.__height))
+        return "\n".join("#" * self.__width for _ in range(self.__height))
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print(str(my_rectangle))
+print(repr(my_rectangle))
+
+print("--")
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle)
+print(repr(my_rectangle))
