81377d09646976361341c08004780d2eb2d77b43
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-23 08:09:50 +0000

    removing test code

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -66,15 +66,3 @@ class Rectangle:
         in an (N)height
         """
         return "\n".join("#" * self.__width for _ in range(self.__height))
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print(str(my_rectangle))
-print(repr(my_rectangle))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
