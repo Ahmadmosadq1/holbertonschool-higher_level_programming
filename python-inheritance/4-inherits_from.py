284f994a6c1fff33ebb93ccfdfe7772a89f46dcb
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-29 18:27:03 +0000

    Task#4: object is not an instnace

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" A function that returns True only if the object is not direct intance
+of its direct class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+
+    """function takes two arguments . obj and a_class
+
+    Arguments:
+            Obj: an instance of a class
+            class: the object
+    raise:
+            Not
+
+    return:
+            True or False
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
