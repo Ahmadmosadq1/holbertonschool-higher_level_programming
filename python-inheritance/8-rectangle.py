ff55e78e07765deedcac0ceae6d2eed4dcf5ccf4
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-31 04:55:05 +0000

    Task#8: using rectangual method

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""ceating BaseGeometry class"""
+
+
+class BaseGeometry:
+    """ A class with no methods only Pass"""
+    def area(self):
+        """a method raises an exception error if called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """height and width validation: the number must be integer and > 0"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+"""Creating a class that inherits from BaseGeometry"""
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        """creating init method to create intances"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
