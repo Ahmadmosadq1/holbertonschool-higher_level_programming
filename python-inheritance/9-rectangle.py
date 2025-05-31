2d2dd40c2d0298c102482867415dab01f2c58cf0
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-31 05:29:47 +0000

    Task#9: calculating the area of the rectangualr

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""ceating BaseGeometry class"""
+
+
+class BaseGeometry:
+    """ A class with no methods only Pass"""
+    def area(self):
+        """a method raises an exception error if called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """height and width validation: the number must be integer and > 0"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Creating a class that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """creating init method to create intances"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculate and return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the area of the rectangular in a string."""
+        return f"[Rectangle] {self.__width}/{self.__height}"
