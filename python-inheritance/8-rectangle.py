b1b5846c984d2199c1726c08d4d74cf9a2a0bff1
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-05-31 05:15:25 +0000

    removing the test code

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -24,4 +24,3 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-print(issubclass(Rectangle, BaseGeometry))
