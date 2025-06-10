95668c25d3f25aa277e31de7ca68eb41c24c7808
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-06-10 21:20:10 +0000

    Task#12: Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+def pascal_triangle(n):
+    """
+    Returns a list of lists representing Pascal's triangle of n rows.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+
+        for j in range(1, len(prev_row)):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
