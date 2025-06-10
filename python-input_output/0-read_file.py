b76946888bd7fde4f8b53876aea37f1759359a83
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-06-10 02:57:29 +0000

    opeining and reading a file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A function that reads a file"""
+
+
+def read_file(filename=""):
+    """
+    Reads a file and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
+
+    Returns:
+        None
+    """
+    with open(filename, 'r') as f:
+        """opeining a file in a read mode"""
+        f.read()
