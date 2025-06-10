23b0ca66c302f58f0ee8a7d10d0189207572b7dd
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-06-10 04:37:21 +0000

    Task#6:loading a Jason file into python file object

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a function that creates an Object from a “JSON file”"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    returns an object from JSON file
+
+    Args:
+        filename: the JSON file
+    Returns:
+        string
+    """
+    with open(filename, 'w') as my_file:
+        return json.load(filename)
