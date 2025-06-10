33438e3f3e1194aad11d449f231ccbc1aa37a4ec
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-06-10 04:07:54 +0000

    Task#4: JSON object to python object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a function that returns an object (Python data structure) 
+represented by a JSON string"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    returns an object from JSON to python object
+
+    Args:
+        my_obj (void): the object used
+    Returns:
+        string
+    """
+    return json.loads(my_str)
