6a6226f361caa2737b592ff19cb06598182ca5b5
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-06-11 22:04:04 +0000

    adding EOFError

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -36,5 +36,5 @@ class CustomObject:
         try:
             with open(filename, 'rb') as f:
                 return pickle.load(f)
-        except(FileNotFoundError, pickle.UnpicklingError):
+        except(FileNotFoundError, pickle.UnpicklingError, EOFError):
             return None
