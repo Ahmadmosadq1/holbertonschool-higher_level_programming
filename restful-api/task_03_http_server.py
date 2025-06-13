2287b78b14d8d782a0c71057c3a9fcb4b239ee6e
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-06-13 02:49:19 +0000

    fixing the 404 string error

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -44,7 +44,7 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.send_header("Content-Type", "text/plain")
             self.send_header("Content-Length", "0")
             self.end_headers()
-            self.wfile.write("404 Not Found")
+            self.wfile.write("Endpoint not found")
 def running(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
     """
     this method runs the web server
