f394bc783f122d6085d51cb555b117eacad5272d
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-06-28 01:29:33 +0000

    adding id

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,6 @@
--- Listing cities of cali
-SELECT name
+SELECT id, name
 FROM cities
 WHERE state_id = (
-  SELECT id
-  FROM states
-  WHERE name = 'California'
+  SELECT id FROM states WHERE name='California'
 )
 ORDER BY id;
