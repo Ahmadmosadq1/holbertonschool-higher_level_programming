7b9076087e456b2ca9d64690f78dc931468cfdb3
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-06-28 05:11:00 +0000

    Task#16

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,17 @@
+-- shows by generes
+SELECT
+  tv_shows.title  AS title,
+  tv_genres.name  AS genre
+FROM
+  tv_shows
+LEFT JOIN
+  tv_show_genres
+    ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN
+  tv_genres
+    ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY
+  tv_shows.title,
+  tv_genres.name;
+
+name
