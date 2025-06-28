1f0be6f656df243e67772adeda4019f3b047a582
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-06-28 01:59:18 +0000

    Task#9:listing cities by states

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,6 @@
 -- listing cities by states
-SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa
+SELECT cities.id, cities.name, states.name 
+FROM hbtn_0d_usa
+JOIN states 
+  ON cities.state_id = states.id
 ORDER BY cities.id
