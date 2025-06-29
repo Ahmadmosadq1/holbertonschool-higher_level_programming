eceec1c18158382206cdb2465be8a6c6f10ad6a1
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-06-29 06:18:25 +0000

    pycodestyle correction

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,11 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
     # they expect this exact query
-    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
+    query = (
+        "SELECT * FROM states "
+        "WHERE name = '{}' "
+        "ORDER BY id ASC"
+            ).format(sys.argv[4])
     for state in cursor.fetchall():
         """fetchall gets the results in a tuple(number, state)
         we should focus on the stats index...that is tuple[1]
