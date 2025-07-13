9e156f01c43eae3a6859cda07b1c5fd59f1064da
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-07-13 05:34:45 +0000

    Task#2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 //  importing argv from process libaray
 const { argv } = require('node:process');
-if (argv.length > 2) {
+if (argv.length > 3) {
+  console.log('Arguments found');
+} else if (argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('No argument');
