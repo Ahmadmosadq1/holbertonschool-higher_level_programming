5e38fb864173ef29dd4999c4f18cd478f999836c
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-07-13 07:24:53 +0000

    Task#6

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const sentance = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let letter = 0; letter < sentance.length; letter++) {
+  console.log(sentance[letter]);
+}
