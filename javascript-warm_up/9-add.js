4f23e45cec84775c2cc2b3a41f8c121925e2540a
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-07-13 08:08:32 +0000

    Task#9

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function add (a, b) {
+  console.log(a + b);
+}
+const { argv } = require('node:process');
+if (!argv[3]) {
+  console.log('NAN');
+} else {
+  const a = Number(argv[2]);
+  const b = Number(argv[3]);
+  add(a, b);
+}
