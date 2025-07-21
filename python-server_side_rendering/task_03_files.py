9cff088ca74c8a603458c2fc05537d986facb073
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-07-21 13:38:42 +0300

    merging

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,32 @@
+import json
+from flask import Flask, render_template
+
+with open('items.json', 'r') as file:
+    content = json.load(file)
+item_list = content.get('items', [])
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+    
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def item():
+    return render_template('items.html', items = item_list)
+
+if __name__ == '__main__':
+     app.run(debug=True, port=5000, use_reloader = False)
+
+
+
